f5d09267c43e533df0f5fd2d90d8ea45c30dba8d
Rmdmostakim <simplemostak@gmail.com>
2023-03-23 06:23:54 +0600

    job retouched

--- app/Jobs/PostCreator.php
+++ app/Jobs/PostCreator.php
@@ -2,13 +2,16 @@
 
 namespace App\Jobs;
 
+use App\Models\Post;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Events\JobProcessed;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Bus;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class PostCreator implements ShouldQueue
 {
@@ -33,15 +36,25 @@ class PostCreator implements ShouldQueue
      * @return void
      */
     public function handle()
+
     {
+        $thumb =  new ThumbCreator($this->file);
+        $low =  new ConvertLowQuality($this->file);
+        $mid =  new ConvertMidQuality($this->file);
+        $high =  new ConvertHighQuality($this->file);
+        $master = new MasterPlaylist($this->file);
         Bus::chain(
             [
-                new ThumbCreator($this->file),
-                new ConvertLowQuality($this->file),
-                new ConvertMidQuality($this->file),
-                new ConvertHighQuality($this->file),
-                new MasterPlaylist($this->file),
+                $thumb, $low, $mid, $high, $master,
             ]
         )->dispatch();
+        Event::listen(JobProcessed::class, function (JobProcessed $event) use ($low, $master) {
+            if ($event->job->resolveName() === get_class($low)) {
+                Post::where('id', 1)->update(['playlist' => 'low']);
+            }
+            if ($event->job->resolveName() === get_class($master)) {
+                Post::where('id', 1)->update(['playlist' => 'master']);
+            }
+        });
     }
 }
