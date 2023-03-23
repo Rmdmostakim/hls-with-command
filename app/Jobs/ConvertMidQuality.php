095b55b5f0e5d2a42645f5d0ccacb7699d4af28b
Rmdmostakim <simplemostak@gmail.com>
2023-03-23 15:21:48 +0600

    filmidlevel converter res.. corrected

--- app/Jobs/ConvertMidQuality.php
+++ app/Jobs/ConvertMidQuality.php
@@ -34,8 +34,9 @@ class ConvertMidQuality implements ShouldQueue
      */
     public function handle()
     {
-        $resolution = '1280x720';
+        $resolution = '854x480';
         $filename = 'public/' . $this->name . '/' . $this->name . '_' . $resolution . '.m3u8';
         $command = "ffmpeg -i $this->file -profile:v baseline -level 3.0 -r 30 -s $resolution -start_number 0 -b:v 1000k -maxrate 1000k -bufsize 2000k -hls_time 10 -hls_list_size 0 -f hls $filename";
+        shell_exec($command);
     }
 }
