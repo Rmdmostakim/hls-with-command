5bccef7d0bdae4398d8fe1b0b95b34b5937e5628
Rmdmostakim <simplemostak@gmail.com>
2023-03-27 12:24:40 +0600

    get all feed merchant and product relation created

--- app/Services/FeedRepositoryServices.php
+++ app/Services/FeedRepositoryServices.php
@@ -128,27 +128,10 @@ class FeedRepositoryServices implements FeedRepositoryInterface
     public function getAllFeed()
     {
         return Feed::with(
-
             'merchant.info:merchant_uuid,company_logo',
-
             'product:id,uuid,name',
-
             'product.details:product_uuid,price,cover,stock,discount,discount_type,discount_duration',
-
             'product.details.cover',
-
-            'gallery',
-
-            'like',
-
-            'comment.userInfo:user_uuid,user_name',
-
-            'comment.profile:user_uuid,path',
-
-            'comment.reply.userInfo:user_uuid,user_name',
-
-            'comment.reply.profile:user_uuid,path',
-
-        )->orderBy('id', 'DESC')->paginate(30);
+        )->orderBy('id', 'DESC')->paginate(10);
     }
 }
